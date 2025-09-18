dc24c36de98abb2d9fd81ffc9ba76557bbf56485
VBonamassa3 <vbonamassa@proton.me>
2025-09-18 14:42:51 -0400

    starting the construction of the account page

---
+++ client-app/src/components/AccountPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const AccountPage = () => {
+  return (
+    <div id="Test" class="">
+      <h3> Event Tracker App </h3>
+      <h2> 😬 </h2>
+      <p>Will be the account page once developed</p>
+    </div>
+  );
+}

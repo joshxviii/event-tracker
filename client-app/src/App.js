53289e888d59bd74eca45dce32215eeea879f63b
Josh <125091065+joshxviii@users.noreply.github.com>
2025-09-22 11:02:01 -0400

    Added boiler plate App.js logic

---
+++ client-app/src/App.js
@@ -0,0 +1,41 @@
+import logo from './assets/logo.svg';
+import './styles/App.css';
+import React, { useState } from 'react';
+import { LogInPage } from './components/LogInPage';
+import { HomePage } from './components/HomePage';
+import { AccountPage } from './components/AccountPage';
+import { EventPage } from './components/EventPage';
+
+export function App() {
+    /* 
+    the 'states' for the app.
+    depending on the combination of these states the app will show different pages.
+    */
+    const [isLoggedIn, setLoggedIn] = useState(false)
+    const [currentPage, setCurrentPage] = useState('home')
+    const [selectedEventId, setSelectedEventId] = useState(0)
+
+    // if the user is not logged in display login page.
+    if(!isLoggedIn) return (
+        <LogInPage />
+    ) 
+
+    // display other pages otherwise.
+    return (
+        <div>
+            <NavigationBar />
+
+            {currentPage === 'home' && (
+                <HomePage />
+            )}
+
+            {currentPage === 'account' && (
+                <AccountPage />
+            )}
+
+            {currentPage === 'event' && (
+                <EventPage />
+            )}
+        </div>
+    );
+}
--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import { LogInPage } from './components/LogInPage'
+import { App } from './App'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <LogInPage />
+    <App />
 
   </React.StrictMode>
 );

039f16258816e8a85752cb005dbddc81e81066d9
Josh <122854644+joshesherr@users.noreply.github.com>
2025-10-07 13:17:07 -0400

    backend init commit

--- client-app/README.md
+++ client-app/README.md
@@ -4,4 +4,5 @@
 
 1. Run `npm install`.
 2. Run `npm install '@vis.gl/react-google-maps'`.
-3. Add API key `.env.local` to this `client-app` folder.
+3. Run `npm install '@react-oauth/google'`
+4. Add API key `.env.local` to this `client-app` folder.
--- client-app/src/App.js
+++ client-app/src/App.js
@@ -10,7 +10,7 @@ export function App() {
     the 'states' for the app.
     depending on the combination of these states the app will show different pages.
     */
-    const [isLoggedIn, setLoggedIn] = useState(true)
+    const [isLoggedIn, setLoggedIn] = useState(false)
     const [currentPage, setCurrentPage] = useState('home') // 'home', 'account', 'event'
     const [selectedEventId, setSelectedEventId] = useState(0)
     

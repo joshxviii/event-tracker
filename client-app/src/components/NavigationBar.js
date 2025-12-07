977022a9f4d422fc625a15eb6df347e7449feddd
Josh <125091065+joshxviii@users.noreply.github.com>
2025-12-07 17:25:00 -0500

    added role field to auth

--- client-app/src/components/NavigationBar.js
+++ client-app/src/components/NavigationBar.js
@@ -24,7 +24,7 @@ export function NavigationBar( { onLogout } ) {
         (async () => {
             setUser(await getCurrentUser());
         })();
-    }, []);
+    }, [isLoggedIn]);
 
     return (
         <nav id='nav-bar'>
